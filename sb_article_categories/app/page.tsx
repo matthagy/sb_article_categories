6fa9c268bb065c220425f3ada9b5a5d3500dd3ea
Matt Hagy <matthew.hagy@gmail.com>
2024-12-16 21:47:39 -0500

    Move articles state to a separate JSON file

--- sb_article_categories/app/page.tsx
+++ sb_article_categories/app/page.tsx
@@ -3,14 +3,21 @@
 import {ArticlesTable} from "@/app/ArticlesTable/ArticlesTable";
 import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import theme from './theme';
+
+const queryClient = new QueryClient();
+
 
 export default function Home() {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <ArticlesTable/>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <ArticlesTable/>
+            </ThemeProvider>
+        </QueryClientProvider>
+
     );
 }
 
